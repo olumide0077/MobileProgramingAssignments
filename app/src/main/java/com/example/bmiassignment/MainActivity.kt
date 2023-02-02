3488ae052a1a795dfb91e9394172f3aff3349b3a
olumide0077 <amosunolumidejoseph@gmail.com>
2023-02-02 18:34:28 +0100

    Body Mass Index Calculator

---
+++ app/src/main/java/com/example/bmiassignment/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.bmiassignment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.bmiassignment.ui.theme.BMIASSIGNMENTTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BMIASSIGNMENTTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Bmi()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Bmi() {
+    var heightInput: String by remember { mutableStateOf("")}
+    var weightInput: String by remember { mutableStateOf("") }
+    val height = heightInput.toFloatOrNull() ?: 0.0f
+    val weight = weightInput.toIntOrNull() ?: 0
+    val bmi = if(weight > 0 && height > 0) weight / (height * height) else 0.0
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    )  {
+        Text(
+            text = "Body mass index",
+            fontSize = 24.sp,
+            color = MaterialTheme.colors.primary,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 16.dp)
+        )
+        OutlinedTextField(
+            value = heightInput,
+            onValueChange = {heightInput = it.replace(',','.')},
+            label = { Text("Height")},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = weightInput,
+            onValueChange = {weightInput = it.replace(',','.')},
+            label = { Text(text = "Weight")},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(text = "Body mass index is " + bmi)
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BMIASSIGNMENTTheme {
+        Bmi()
+    }
+}
